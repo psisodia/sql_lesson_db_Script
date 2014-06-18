8e97fecd52f22b643aabdc0cb122a713c52baa72
Hackbright Student <info@hackbrightacademy.com>
2014-06-17 17:38:44 -0700

    Adding changed files

--- hackbright_app.py
+++ hackbright_app.py
@@ -7,9 +7,9 @@ def get_student_by_github(github):
     query = """SELECT first_name, last_name, github FROM Students WHERE github = ?"""
     DB.execute(query, (github,))
     row = DB.fetchone()
-    return """\
-Student: %s %s
-Github account: %s"""%(row[0], row[1], row[2])
+    return row
+#Student: %s %s
+#Github account: %s"""%(row[0], row[1], row[2])
 
 def make_new_student(first_name,last_name,github):
     query = """INSERT into Students values(?,?,?)"""
--- webapp.py
+++ webapp.py
@@ -6,10 +6,26 @@ app = Flask(__name__)
 #code goes here
 
 @app.route("/")
+def get_github():
+    return render_template("get_github.html")
+
+@app.route("/student")
 def get_student_by_github():
     hackbright_app.connect_to_db()
     student_github = request.args.get("student")
-    return hackbright_app.get_student_by_github(student_github)
-    
+    row = hackbright_app.get_student_by_github(student_github)
+    html = render_template("student_info.html", first_name=row[0],
+                                                last_name=row[1],
+                                                github=row[2])
+    return html
+
+@app.route("/student")
+def get_student_grades():
+    hackbright_app.connect_to_db()
+    student_github = request.args.get("student")
+    row1 = hackbright_app.get_student_grades(student_github)
+    html = render_template("student_info.html", project_title=row1[0],
+                                                grades=row1[1])
+    return html
 if __name__ == "__main__":
     app.run(debug=True)
